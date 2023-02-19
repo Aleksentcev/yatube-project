3a02340108385de40c790e4dd7274527d0a52c68
Михаил Алексенцев <legolas961@gmail.com>
2023-02-19 15:54:37 +0300

    Visual changes

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.aleksentcev.pythonanywhere.com',
+    'aleksentcev.pythonanywhere.com',
 ]
 
 # Application definition
